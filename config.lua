3e5c05c0089fc1d39c3106311c1614c55f79c55c
GG79 <midlifegamingcrisis@gmail.com>
2022-12-30 08:34:27 +0200

    Added NPC

--- config.lua
+++ config.lua
@@ -6,7 +6,8 @@ Config.Mysql = "oxmysql" -- mysql-async -- oxmysql
 Config.SetCraftXpCommand = 'setxp' -- example = setxp id xp  = setxp 2 5
 Config.AdminPerm = 'admin' --- admin rank
 Config.Craft = {
-    {npcHash = 's_m_m_ammucountry' ,coords = vector3(2306.89, 4881.82, 41.8082) , npcHeading = 31.91}
+    {npcHash = 's_m_m_ammucountry', coords = vector3(2306.89, 4881.82, 41.8082) , npcHeading = 31.91},
+    {npcHash = 's_m_y_chef_01', coords = vector3(-590.45, -1056.52, 22.36) , npcHeading = 97.45}
 }
 
 
@@ -23,6 +24,10 @@ Config.Categories = {
     {
         name = "tools",
         label = "Tools",
+    },
+    {
+        name = "food",
+        label = "Food",
     },
   
 }
