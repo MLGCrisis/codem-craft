54ddddeda3fc7b99ee242e571da93761359ba652
GG79 <midlifegamingcrisis@gmail.com>
2023-03-23 14:24:41 +0200

    Added crafting items

--- config.lua
+++ config.lua
@@ -34,20 +34,175 @@ Config.Categories = {
 
 Config.CraftItem = {
     {
-        itemName = 'weapon_assaultrifle_mk2',
-        itemLabel = 'Assault Rifle',
+        itemName = 'handcuffs',
+        itemLabel = 'Handcuffs',
+        minute = 5 ,
+        level = 0,
+        xp = 100,
+        category = 'tools',
+        required = {
+            { label = 'Steel', name = "steel", amount = 10},
+            { label = 'Plastic', name = "plastic", amount =10},
+            
+        },
+        imagesname = 'handcuffs' ,  ---- for mid image
+   
+    },
+    {
+        itemName = 'weapon_combatpistol',
+        itemLabel = 'Combat Pistol',
         minute = 20 ,
         level = 0,
         xp = 100,
         category = 'weapon',
         required = {
-            { label = 'Scrab', name = "tosti", amount = 10},
-            { label = 'Aluminum', name = "tosti", amount =10},
-            { label = 'Iron', name = "tosti", amount =10},
+            { label = 'Plastic', name = "plastic", amount = 10},
+            { label = 'Metal Scrap', name = "metalscrap", amount =10},
+            { label = 'Steel', name = "steel", amount =10},
             
         },
-        imagesname = 'Assault-Rifle-MK-II-Big' ,  ---- for mid image
+        imagesname = 'weeapon_combatpistol' ,  ---- for mid image
    
+    },
+    {
+        itemName = 'weapon_heavypistol',
+        itemLabel = 'Heavy Pistol',
+        minute = 20 ,
+        level = 0,
+        xp = 100,
+        category = 'weapon',
+        required = {
+            { label = 'Plastic', name = "plastic", amount = 20},
+            { label = 'Metal Scrap', name = "metalscrap", amount =20},
+            { label = 'Steel', name = "steel", amount =20},
+            { label = 'Screwdriver Set', name = "screwdriverset", amount =1},
+        },
+        imagesname = 'weapon_heavypistol' ,  ---- for mid image
+   
+    },
+    {
+        itemName = 'weapon_stickybomb',
+        itemLabel = 'Sticky Bomb',
+        minute = 30 ,
+        level = 1,
+        xp = 200,
+        category = 'weapon',
+        required = {
+            { label = 'Aluminum Oxide', name = "aluminumoxide", amount = 50},
+            { label = 'Copper', name = "copper", amount = 50},
+            { label = 'Iron Oxide', name = "ironoxide", amount =50},
+            { label = 'Steel', name = "steel", amount =50},
+            { label = 'Screwdriver Set', name = "screwdriverset", amount =1},
+            { label = 'Electronic Kit', name = "electronickit", amount =1},
+            { label = 'Thermite', name = "thermite", amount =1},
+            
+        },
+        imagesname = 'weapon_stickybomb' ,  ---- for mid image
+   
+    },
+    {
+        itemName = 'weapon_molotov',
+        itemLabel = 'Sticky Bomb',
+        minute = 30 ,
+        level = 1,
+        xp = 200,
+        category = 'weapon',
+        required = {
+            { label = 'Bottle', name = "aluminumoxide", amount = 1},
+            { label = 'Lighter', name = "copper", amount = 1},
+            { label = 'Jerry Can', name = "ironoxide", amount =1},
+            
+        },
+        imagesname = 'weapon_molotov' ,  ---- for mid image
+   
+    },
+    {
+        itemName = 'weapon_assaultsmg',
+        itemLabel = 'Assault SMG',
+        minute = 30 ,
+        level = 1,
+        xp = 200,
+        category = 'weapon',
+        required = {
+            { label = 'Plastic', name = "plastic", amount = 50},
+            { label = 'Iron', name = "iron", amount =50},
+            { label = 'Steel', name = "steel", amount =50},
+            { label = 'Screwdriver Set', name = "screwdriverset", amount =1},
+            
+        },
+        imagesname = 'weapon_assaultsmg' ,  ---- for mid image
+   
+    },
+    {
+        itemName = 'weapon_bullpupshotgun',
+        itemLabel = 'Bullpup Shotgun',
+        minute = 30 ,
+        level = 2,
+        xp = 200,
+        category = 'weapon',
+        required = {
+            { label = 'Plastic', name = "plastic", amount = 50},
+            { label = 'Rubber', name = "rubber", amount = 20},
+            { label = 'Iron', name = "iron", amount =50},
+            { label = 'Steel', name = "steel", amount =50},
+            { label = 'Screwdriver Set', name = "screwdriverset", amount =1},
+            
+        },
+        imagesname = 'weapon_bullpupshotgun' ,  ---- for mid image
+    },
+    {
+        itemName = 'weapon_compactrifle',
+        itemLabel = 'Compact Rifle',
+        minute = 30 ,
+        level = 2,
+        xp = 200,
+        category = 'weapon',
+        required = {
+            { label = 'Plastic', name = "plastic", amount = 50},
+            { label = 'Rubber', name = "rubber", amount = 20},
+            { label = 'Iron', name = "iron", amount =50},
+            { label = 'Aluminum', name = "aluminum", amount =50},
+            { label = 'Screwdriver Set', name = "screwdriverset", amount =1},
+            
+        },
+        imagesname = 'weapon_compactrifle' ,  ---- for mid image
+    },
+    {
+        itemName = 'weapon_combatmg',
+        itemLabel = 'Compact Rifle',
+        minute = 40 ,
+        level = 3,
+        xp = 300,
+        category = 'weapon',
+        required = {
+            { label = 'Plastic', name = "plastic", amount = 50},
+            { label = 'Copper', name = "copper", amount = 50},
+            { label = 'Rubber', name = "rubber", amount = 20},
+            { label = 'Iron', name = "iron", amount =50},
+            { label = 'Aluminum', name = "aluminum", amount =50},
+            { label = 'Screwdriver Set', name = "screwdriverset", amount =1},
+            
+        },
+        imagesname = 'weapon_combatmg' ,  ---- for mid image
+    },
+    {
+        itemName = 'weapon_marksmanrifle',
+        itemLabel = 'Compact Rifle',
+        minute = 60 ,
+        level = 4,
+        xp = 500,
+        category = 'weapon',
+        required = {
+            { label = 'Plastic', name = "plastic", amount = 50},
+            { label = 'Metal Scrap', name = "metalscrap", amount = 50},
+            { label = 'Copper', name = "copper", amount = 50},
+            { label = 'Rubber', name = "rubber", amount = 20},
+            { label = 'Iron', name = "iron", amount =50},
+            { label = 'Aluminum', name = "aluminum", amount =50},
+            { label = 'Screwdriver Set', name = "screwdriverset", amount =1},
+            
+        },
+        imagesname = 'weapon_marksmanrifle' ,  ---- for mid image
     },
     {
         itemName = 'rifle_ammo',
